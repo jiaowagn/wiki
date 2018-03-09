1144224b35a3906e684a52276626ddc80f61e9fc
jiaowagn <wangjiaohere@gmail.com>
2018-03-09 10:15:54 +0800

    add association between article and user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all.order("created_at DESC")
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       redirect_to @article
     else
